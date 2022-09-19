0e204e1d50079d831659a344b86a0a552905a5f7
LinaCart <catahecarica22@hotmail.com>
2022-09-18 21:19:44 -0500

    Añadir LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css';
 import { Formulario } from "./componentes/Formulario"
 import { Tarea } from './componentes/Tarea'
@@ -8,6 +8,12 @@ function App() {
   const [tarea, setTarea] = useState('')
   const [fecha, setFecha] = useState('')
   const [listadoTareas, setListadoTareas] = useState([])
+
+  useEffect(function () {
+    setListadoTareas(JSON.parse(localStorage.getItem("tareas")))
+  }, [])
+
+
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -26,6 +32,9 @@ function App() {
 
     const temp = [nuevaTarea, ...listadoTareas]
     setListadoTareas(temp)
+    localStorage.setItem("tareas", JSON.stringify(temp));
+
+
 
     setTarea('')
 
@@ -43,53 +52,57 @@ function App() {
     console.log(tarea)
   }
 
-    function onActualizarTarea(objEditarTarea) {
-      const { id, tarea } = objEditarTarea
+  function onActualizarTarea(objEditarTarea) {
+    const { id, tarea } = objEditarTarea
 
-      const temp = [...listadoTareas]
-      const elemento = temp.find(item => item.id === id)
-      elemento.tarea = tarea
+    const temp = [...listadoTareas]
+    const elemento = temp.find(item => item.id === id)
+    elemento.tarea = tarea
+    localStorage.setItem("tareas", JSON.stringify(temp));
 
-      setListadoTareas(temp)
-    }
 
-    function onBorrarTarea(id) {
-      const temp = listadoTareas.filter(item => item.id !== id)
-      setListadoTareas(temp)
-    }
-
-    return (
-      <>
-        <div className='contenedorPrincipal'>
-          <h1>To-Do List</h1>
-          <div className='contenedorFormulario'>
-            <Formulario
-              tarea={tarea}
-              handleSubmit={handleSubmit}
-              handleChange={handleChange}
-              handleChangeFecha={handleChangeFecha} 
-              fecha={fecha}
-              />
-          </div>
-
-          <div className='contenedorTareas'>
-            <h2>Lista De Tareas</h2>
-            <div className='contenedorInfoTareas'>
-              {
-                listadoTareas.map(tarea => (
-                  <Tarea
-                    key={tarea.id}
-                    id={tarea.id}
-                    tarea={tarea}
-                    onActualizarTarea={onActualizarTarea}
-                    onBorrarTarea={onBorrarTarea} />
-                ))
-              }
-            </div>
-          </div>
-        </div>
-      </>
-    );
+    setListadoTareas(temp)
   }
 
-  export default App;
+  function onBorrarTarea(id) {
+    const temp = listadoTareas.filter(item => item.id !== id)
+    setListadoTareas(temp)
+    localStorage.setItem("tareas", JSON.stringify(temp));
+
+  }
+
+  return (
+    <>
+      <div className='contenedorPrincipal'>
+        <h1>To-Do List</h1>
+        <div className='contenedorFormulario'>
+          <Formulario
+            tarea={tarea}
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            handleChangeFecha={handleChangeFecha}
+            fecha={fecha}
+          />
+        </div>
+
+        <div className='contenedorTareas'>
+          <h2>Lista De Tareas</h2>
+          <div className='contenedorInfoTareas'>
+            {
+              listadoTareas.map(tarea => (
+                <Tarea
+                  key={tarea.id}
+                  id={tarea.id}
+                  tarea={tarea}
+                  onActualizarTarea={onActualizarTarea}
+                  onBorrarTarea={onBorrarTarea} />
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
